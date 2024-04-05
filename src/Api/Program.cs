58da2990388107cf3c816c1b4ea4581ec8bc2361
Lasse Jonassen <lassemoellerjonassen@gmail.com>
2024-04-05 18:22:16 +0200

    Implementation of multiple environments. Closes #18 and #2

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,10 @@
+using System;
+using System.IO;
 using Api;
 using Application;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 internal class Program
@@ -9,15 +12,26 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
-		{
-			builder.Services
+
+		var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+		var configBuilder = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("Configuration/appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"Configuration/appsettings.{environment}.json", optional: true, reloadOnChange: true)
+			.AddEnvironmentVariables();
+
+		var configuration = configBuilder.Build();
+		builder.Configuration.AddConfiguration(configuration);
+
+		builder.Services
 				.AddPresentation()
 				.AddApplication()
 				.AddInfrastructure(builder.Configuration);
-		}
 
 		var app = builder.Build();
 		{
+
 			app.UseExceptionHandler();
 			app.UseInfrastructure();
 
