d058416b1f0570da7496e7fdc402ff37cddf27f4
Lasse Jonassen <lassemoellerjonassen@gmail.com>
2024-04-07 23:35:14 +0200

    Implemented Auditlogging directly in the delete-handler for messages.

--- src/Application/Messages/Commands/Delete/DeleteMessageCommandHandler.cs
+++ src/Application/Messages/Commands/Delete/DeleteMessageCommandHandler.cs
@@ -1,11 +1,14 @@
-﻿using Application.Common.Interfaces;
+﻿using System.Text.Json;
+using Application.Common.Interfaces;
 using Contracts.Messages;
+using Domain.Audit;
+using Domain.Messages;
 using ErrorOr;
 using MediatR;
 
 namespace Application.Messages.Commands;
 
-public class DeleteMessageCommandHandler(IMessagesRepository _repository) 
+public class DeleteMessageCommandHandler(IMessagesRepository _repository, IAuditLogsRepository auditRepo) 
 	: IRequestHandler<DeleteMessageCommand, ErrorOr<MessageResponse>>
 {
 	public async Task<ErrorOr<MessageResponse>> Handle(DeleteMessageCommand command, CancellationToken cancellationToken)
@@ -18,6 +21,20 @@ public class DeleteMessageCommandHandler(IMessagesRepository _repository)
 		}
 
 		await _repository.RemoveAsync(message, cancellationToken);
+
+		var auditLog = new AuditLog()
+		{
+			Id = Guid.NewGuid(),
+			CreateTime = DateTimeOffset.Now,
+			CorrelationId = Guid.NewGuid(),
+			EntityType = message.GetType().Name,
+			EntityId = message.Id.ToString(),
+			ChangedByCommand = GetType().Name,
+			CommandId = Guid.NewGuid().ToString(),
+			Content = JsonSerializer.Serialize(message)
+		};
+
+		await auditRepo.CreateAuditLog(auditLog);
 
 		return new MessageResponse(message.Id, message.To, message.From, message.Text, message.Read);
 	}	
